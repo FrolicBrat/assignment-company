169d97f292c768b61a46fafc94254aa5d63dfc9b
Shashwat gangwar <Shashwat1195@gmail.com>
2023-03-31 10:09:20 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Toaster } from "react-hot-toast";
+
+// Components
+import Loading from "./components/Loading";
+
+// Pages
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const TodoPage = lazy(() => import("./pages/TodoPage"));
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuth);
+  const ProtectedRoute = ({ children }) => {
+    if (!isAuth) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/">
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <TodoPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="register" element={<RegisterPage />} />
+          </Route>
+        </Routes>
+        <Toaster
+          toastOptions={{ className: "toaster" }}
+          position="bottom-center"
+        />
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
